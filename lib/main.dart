c70af828564c9fb622ebb8cbd983737d5d65719b
Naruse-Kazuki <kuroraido@icloud.com>
2020-05-04 19:04:24 +0900

    Add_mainAxisAlignment_spaceBetween

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               Container(
                 height: 100.0,
