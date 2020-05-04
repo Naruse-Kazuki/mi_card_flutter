d7e0a1af2f0070152a4bdce065ec9d009b0091f9
Naruse-Kazuki <kuroraido@icloud.com>
2020-05-04 19:01:52 +0900

    Add_mainAxisAlignment_center

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Container(
                 height: 100.0,
