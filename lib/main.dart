980859e432f82ed20752a57b47e6ed556718d37d
Naruse-Kazuki <kuroraido@icloud.com>
2020-05-04 18:04:15 +0900

    Add_fromKTRB

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
           child: Container(
             height: 100.0,
             width: 100.0,
-            margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
+            margin: EdgeInsets.fromLTRB(20.0, 10.0, 50.0, 20.0),
             color: Colors.white,
             child: Text('Hello'),
           ),
