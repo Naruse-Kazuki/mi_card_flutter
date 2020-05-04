826cf7ed76aa6afb339c9d0e3532d513d76f5d1b
Naruse-Kazuki <kuroraido@icloud.com>
2020-05-04 19:01:15 +0900

    Add_mainAxisAlignment_end

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            verticalDirection: VerticalDirection.down,
+            mainAxisAlignment: MainAxisAlignment.end,
             children: <Widget>[
               Container(
                 height: 100.0,
