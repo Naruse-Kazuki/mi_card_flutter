a341a842114752814d3d0896a2d6e1e2c11845ae
Naruse-Kazuki <kuroraido@icloud.com>
2020-05-07 17:06:19 +0900

    CircleAvatar

--- lib/main.dart
+++ lib/main.dart
@@ -11,25 +11,27 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+          child: Column(
             children: <Widget>[
-              Container(
-                width: 30.0,
-                color: Colors.white,
-                child: Text('Container 1'),
-              ),
-              SizedBox(
-                width: 30.0,
-              ),
-              Container(
-                color: Colors.blue,
-                child: Text('Container 2'),
-              ),
-              Container(
-                color: Colors.red,
-                child: Text('Container 3'),
-              ),
+              CircleAvatar(
+                radius: 50.0,
+              )
+//              Container(
+//                width: 30.0,
+//                color: Colors.white,
+//                child: Text('Container 1'),
+//              ),
+//              SizedBox(
+//                width: 30.0,
+//              ),
+//              Container(
+//                color: Colors.blue,
+//                child: Text('Container 2'),
+//              ),
+//              Container(
+//                color: Colors.red,
+//                child: Text('Container 3'),
+//              ),
             ],
           ),
         ),
