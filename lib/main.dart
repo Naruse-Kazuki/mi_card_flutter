1ba8629ef19a1daf0fab38686b1e8123906d49b9
Naruse-Kazuki <kuroraido@icloud.com>
2020-05-07 17:26:38 +0900

    Add_TextStyle

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,14 @@ class MyApp extends StatelessWidget {
                 radius: 50.0,
                 backgroundImage: AssetImage(
                     'image/Screenshot_20200322-023249~01_20200325104138.png'),
-              )
+              ),
+              Text(
+                'Kazuki Naruse',
+                style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
             ],
           ),
         ),
